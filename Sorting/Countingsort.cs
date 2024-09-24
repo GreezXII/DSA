d737ae12fb602359bfbaedcfd8eb8a2204d62369
Kirill Novikov <kirill.novikov.dev@gmail.com>
2024-09-24 20:57:47 +0300

    Couningsort

---
+++ Sorting/Countingsort.cs
@@ -0,0 +1,59 @@
+﻿namespace Sorting;
+
+class Countingsort
+{
+    public static void Sort(int[] array)
+    {
+        if (array.Length < 1)
+            return;
+        
+        var bounds = FindBounds(array);
+        if (bounds is null)
+            return;
+
+        var min = bounds.Value.Min;
+        var max = bounds.Value.Max;
+        var length = max - min + 1; // + 1 for zero
+        var counter = new int[length];
+
+        for (var i = 0; i < length; i++)
+        {
+            var index = array[i] - min;
+            counter[index]++;
+        }
+
+        for (var i = 0; i < length; i++)
+        {
+            if (counter[i] == 0)
+                continue;
+
+            for (var j = 0; j < counter[i]; j++)
+            {
+                var value = i + min;
+                array[i + j] = value;
+            }
+        }
+    }
+
+    private static Bounds? FindBounds(int[] array)
+    {
+        if (array.Length < 1)
+            return null;
+        
+        var bounds = new Bounds(array[0], array[0]);
+        for (var i = 1; i < array.Length; i++)
+        {   
+            if (array[i] > bounds.Max)
+                bounds.Max = array[i];
+            if (array[i] < bounds.Min)
+                bounds.Min = array[i];
+        }
+        return bounds;
+    }
+}
+
+internal struct Bounds(int min, int max)
+{
+    public int Min { get; set; } = min;
+    public int Max { get; set; } = max;
+}
--- Tests/Sort.cs
+++ Tests/Sort.cs
@@ -41,6 +41,19 @@ public class Sort
                 Assert.AreEqual(expected[i], values[i]);
         }
     }
+
+    [TestMethod]
+    public void Countingsort_Success()
+    {
+        var q = new int[] { -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        foreach (var values in GeneratePermutations(q, 0, q.Length - 1))
+        {
+            var expected = values.OrderBy(x => x).ToArray();
+            Countingsort.Sort(values);
+            for (var i = 0; i < values.Length - 1; i++)
+                Assert.AreEqual(expected[i], values[i]);
+        }
+    }
     
     static IEnumerable<int[]> GeneratePermutations(int[] arr, int start, int end)
     {
