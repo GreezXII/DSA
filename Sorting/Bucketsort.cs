85b7475e4a339ecb005a377a2dd1fe1b6ac99ea9
Kirill Novikov <kirill.novikov.dev@gmail.com>
2024-09-25 21:40:40 +0300

    bucketsort

---
+++ Sorting/Bucketsort.cs
@@ -0,0 +1,39 @@
+﻿namespace Sorting;
+
+public class Bucketsort
+{
+    public static void Sort(int[] array, int bucketsCounter)
+    {
+        if (bucketsCounter == 0)
+            return;
+        
+        var buckets = new List<int>[bucketsCounter];
+        for (var i = 0; i < buckets.Length; i++)
+            buckets[i] = [];
+        
+        var bounds = SortHelpers.FindBounds(array);
+        if (bounds is null)
+            return;
+        
+        var bucketLength = (Math.Abs(bounds.Value.Min) + Math.Abs(bounds.Value.Max) + 1) / (double)buckets.Length;
+        foreach (var value in array)
+        {
+            var bucketIndex = (int)((value + Math.Abs(bounds.Value.Min)) / bucketLength);
+            var bucket = buckets[bucketIndex];
+            bucket.Add(value);
+        }
+
+        foreach (var bucket in buckets)
+            bucket.Sort();
+
+        var index = 0;
+        foreach (var bucket in buckets)
+        {
+            foreach (var value in bucket)
+            {
+                array[index] = value;
+                index++;
+            }
+        }
+    }
+}
--- Tests/SortTests.cs
+++ Tests/SortTests.cs
@@ -67,6 +67,19 @@ public class SortTests
                 Assert.AreEqual(expected[i], values[i]);
         }
     }
+
+    [TestMethod]
+    public void Bucketsort_Success()
+    {
+        var q = new int[] { -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        foreach (var values in GeneratePermutations(q, 0, q.Length - 1))
+        {
+            var expected = values.OrderBy(x => x).ToArray();
+            Bucketsort.Sort(values, 5);
+            for (var i = 0; i < values.Length - 1; i++)
+                Assert.AreEqual(expected[i], values[i]);
+        }
+    }
     
     static IEnumerable<int[]> GeneratePermutations(int[] arr, int start, int end)
     {
