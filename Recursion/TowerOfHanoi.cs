51e15e304bcfe53a99caf052b18d44c219a6d1d2
Kirill Novikov <kirill.novikov.dev@gmail.com>
2024-11-11 21:32:55 +0300

    Tower of Hanoi

---
+++ Recursion/TowerOfHanoi.cs
@@ -0,0 +1,21 @@
+﻿namespace Recursion;
+
+public class TowerOfHanoi
+{
+    private readonly Action<int, char, char, char> _action;
+    
+    public TowerOfHanoi(Action<int, char, char, char> action)
+    {
+        _action = action;   
+    }
+    
+    public void Do(int level, char from, char to, char aux)
+    {
+        if (level == 0)
+            return;
+        
+        Do(level - 1, from, aux, to);
+        _action(level - 1, from, to, aux);
+        Do(level - 1, aux, to, from);
+    }
+}
---
+++ Tests/RecursionTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Recursion;
+
+namespace Tests;
+
+[TestClass]
+public class RecursionTests
+{
+    [TestMethod]
+    public void TowerOfHanoiTest()
+    {
+        var moves = new List<Move>();
+        
+        var toh = new TowerOfHanoi(
+            (level, from, to, aux) => moves.Add(new Move(level, from, to)));
+        toh.Do(3, 'A', 'C', 'B');
+        
+        CollectionAssert.AreEqual(new List<Move>
+        {
+            new(0, 'A', 'C'),
+            new(1, 'A', 'B'),
+            new(0, 'C', 'B'),
+            new(2, 'A', 'C'),
+            new(0, 'B', 'A'),
+            new(1, 'B', 'C'),
+            new(0, 'A', 'C'),
+        }, moves);
+    }
+    
+    record Move(int Level, char From, char To);
+}
